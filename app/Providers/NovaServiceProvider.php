dfb533da175557167a5736aa85e470d491a63da1
Andres Quintero <aquintero@oru.com.co>
2019-04-14 20:51:16 -0500

    Allow nova access from all users

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -42,9 +42,10 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewNova', function ($user) {
-            return in_array($user->email, [
-                'andresquintero@zeroplatform.com'
-            ]);
+            // return in_array($user->email, [
+            //     'andresquintero@zeroplatform.com'
+            // ]);
+            return true;
         });
     }
 

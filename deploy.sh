a06b0d8e710f120f334e5005ca60f15980b625fc
Andres Quintero <aquintero@oru.com.co>
2019-05-22 12:03:59 -0500

    Improve deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
-
 eval "$(ssh-agent -s)"
 ssh-add -k ~/.ssh/id_rsa_servers
+git pull
 
 sudo supervisorctl reread
 sudo supervisorctl update
-sudo supervisorctl restart gps-worker:*
+sudo supervisorctl restart servers-worker:*
 
 composer install --optimize-autoloader --no-dev
 php artisan config:cache
